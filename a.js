b7f2d495b61c40c51c9c4ddc2ffc54825c5e0c38
Tsunami <39643347+Tsunami2360@users.noreply.github.com>
2023-02-25 23:27:29 +0000

    migrate

--- a.js
+++ a.js
@@ -3,20 +3,5 @@ const dotenv = require("dotenv").config();
 const fs = require("fs");
 const db = require("better-sqlite3")("./test.db", { verbose: console.log });
 
-
-
-const row = db.prepare('SELECT * FROM users WHERE id = ?').get(userId);
-console.log(row.firstName, row.lastName, row.email);
-
-
-db.table("filesystem_directory", {
-  columns: ['filename', 'data'],
-  rows: function* () {
-    for (const filename of fs.readdirSync(process.cwd())) {
-      const data = fs.readFileSync(filename);
-      yield { filename, data };
-    }
-  },
-});
-
-const files = db.prepare('SELECT * FROM filesystem_directory').all();
+const migration = fs.readFileSync('migrate-schema.sql', 'utf8');
+db.exec(migration);

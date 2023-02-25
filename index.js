52e76440b944ced002a1b3389632575cf4c21a7e
Tsunami <39643347+Tsunami2360@users.noreply.github.com>
2023-02-25 14:51:06 +0000

    listener

--- index.js
+++ index.js
@@ -49,5 +49,6 @@ app.get("*", (req, res) => {
 });
 
 const listener = app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  console.log(listener.address());
+  console.log(`⚡️[server]: Server is running at http://0.0.0.0:${PORT}`);
 });

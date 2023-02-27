e57269bd8d93b82db3a7fe5a2c64203aeb2b062a
Tsunami2360 <>
2023-02-27 23:05:00 +0100

    all

--- a.js
+++ a.js
@@ -2,16 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const fs = require("fs");
 const db = require("better-sqlite3")("./test.sqlite3", { verbose: console.log });
-//const sqlite3 = require("better-sqlite3");
 
-/*
-let sdb = new sqlite3("./test.sqlite3", sqlite3.OPEN_READWRITE, (err) => {
-  if(err) console.error(err);
-  verbose: console.log;
-});
-*/
-const migration = fs.readFileSync("insert.sql", "utf8");
-db.exec(migration);
+//const migration = fs.readFileSync("insert.sql", "utf8");
+//db.exec(migration);
 
-//const rows = db.prepare("SELECT * FROM test;").all();
-//rows.forEach(r => console.log(r.username));
+const rows = db.prepare("SELECT * FROM test;").all();
+rows.forEach(r => console.log(r));

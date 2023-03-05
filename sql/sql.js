328febcadd22e23e88b6c27a6be38eb8880383ea
Tsunami2360 <>
2023-03-05 22:25:29 +0100

    final

--- sql/sql.js
+++ sql/sql.js
@@ -4,8 +4,8 @@ const db = require("better-sqlite3")("./itemdb.sqlite3", { verbose: console.log 
 const createtable = fs.readFileSync("createtable.sql", "utf8");
 db.exec(createtable);
 
-const insert = fs.readFileSync("insert.sql", "utf8");
-db.exec(insert);
+//const insert = fs.readFileSync("insert.sql", "utf8");
+//db.exec(insert);
 
 const rows = db.prepare("SELECT * FROM items;").all();
 rows.forEach(r => console.log(r));
